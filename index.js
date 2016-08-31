b5b7a030fc7fe3af96fa357cda351d383d3dc8df
Andrew Batz <abatz@wealthforge.com>
2016-08-30 22:27:25 -0400

    commented out the file version, used prompt. don't use prompt going deeper

--- index.js
+++ index.js
@@ -1,23 +1,47 @@
-var fs = require('fs');
-// Read the contents of the file into memory.
-fs.readFile('data', 'utf8', function (err, fileOut) {
-    if (err) throw err;
+// var fs = require('fs');
+//
+// // Read the contents of the file into memory.
+// fs.readFile('data', 'utf8', function (err, fileOut) {
+//     if (err) throw err;
+//
+//     console.log(fileOut);
+//
+//     var splitFile = fileOut.split('\n');
+//     console.log(splitFile);
+//
+//     var lines = parseInt(splitFile[0]);
+//     console.log(typeof lines);
+//
+//     var l1 = splitFile[1].split(" ").map(x => parseInt(x));
+//     console.log(l1);
+//     console.log(parseLine(l1));
+//
+//
+//
+// });
 
-    console.log(fileOut);
 
-    var splitFile = fileOut.split('\n');
-    console.log(splitFile);
+// high level input;
+var prompt = require('prompt');
 
-    var lines = parseInt(splitFile[0]);
-    console.log(typeof lines);
+prompt.start();
 
-    var l1 = splitFile[1].split(" ").map(x => parseInt(x));
-    console.log(l1);
-    console.log(parseLine(l1));
+prompt.get(['X'], function(err, result){
+    var lines = parseInt(result.X);
+    console.log(lines + typeof lines);
+
+    for (var i = 0; i < lines; i++) {
+        prompt.get(['Y'], function(err, result){
+            console.log(parseLine(formatLine(result.Y)));
+        })
+    }
 
 
+})
 
-});
+var formatLine = (string) =>{
+    return string.split(" ").map(x => parseInt(x));
+};
 
 var parseLine = (line) =>{
     // check to see if rule 2 is followed, not needed
