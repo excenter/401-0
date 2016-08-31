d3799cf6a7d0f5a70621e01e7c209006a2c77f36
Andrew Batz <abatz@wealthforge.com>
2016-08-30 23:21:20 -0400

    this works, doesn't like being input from a file tho

--- index.js
+++ index.js
@@ -1,48 +1,7 @@
-// var fs = require('fs');
-//
-// // Read the contents of the file into memory.
-// fs.readFile('data', 'utf8', function (err, fileOut) {
-//     if (err) throw err;
-//
-//     console.log(fileOut);
-//
-//     var splitFile = fileOut.split('\n');
-//     console.log(splitFile);
-//
-//     var lines = parseInt(splitFile[0]);
-//     console.log(typeof lines);
-//
-//     var l1 = splitFile[1].split(" ").map(x => parseInt(x));
-//     console.log(l1);
-//     console.log(parseLine(l1));
-//
-//
-//
-// });
-
-
-// high level input;
-var prompt = require('prompt');
-
-prompt.start();
-
-prompt.get(['X'], function(err, result){
-    var lines = parseInt(result.X);
-    console.log(lines + typeof lines);
-
-    for (var i = 0; i < lines; i++) {
-        prompt.get(['Y'], function(err, result){
-            console.log(parseLine(formatLine(result.Y)));
-        })
-    }
-
-
-})
-
+var readlineSync = require('readline-sync');
 var formatLine = (string) =>{
     return string.split(" ").map(x => parseInt(x));
 };
-
 var parseLine = (line) =>{
     // check to see if rule 2 is followed, not needed
     if(line.length-1 != line[0]){
@@ -50,3 +9,8 @@ var parseLine = (line) =>{
     }
     return line[line[line.length-1]]+line[line[line.length-2]];
 };
+
+var linez = parseInt(readlineSync.question(''));
+for (var i = 0; i < linez; i++) {
+    console.log(parseLine(formatLine(readlineSync.question(''))));
+}
