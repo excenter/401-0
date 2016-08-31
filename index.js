10aef97771840756805ee95c31ec8c5d0f84be1e
Andrew Batz <abatz@wealthforge.com>
2016-08-30 21:51:04 -0400

    parseLine is a gorgeous example of a 'big arrow', 'anonymous', 'complex' function. so sexy. look at that

--- index.js
+++ index.js
@@ -13,14 +13,18 @@ fs.readFile('data', 'utf8', function (err, fileOut) {
 
     var l1 = splitFile[1].split(" ").map(x => parseInt(x));
     console.log(l1);
-    console.log(parseLine('x'));
+    console.log(parseLine(l1));
 
 
 
 });
 
 var parseLine = (line) =>{
-    return line + 'x';
+    // check to see if rule 2 is followed, not needed
+    if(line.length-1 != line[0]){
+        return 'ERROR YO';
+    }
+    return line[line[line.length-1]]+line[line[line.length-2]];
 };
 
 // scope.accord = () => {
